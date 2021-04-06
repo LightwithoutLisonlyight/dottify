463752804f9a93d2ad47e04d06ff56ecaf948f6d
lightwl <lightwl@outlook.com>
2021-04-06 14:58:16 +0200

    Init

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+exports.dottify = dottify
+
+
+function isObject(test){return test && typeof test === 'object'}
+
+function dottify(object,separator = '.'){
+    try{
+    let flatted = {}
+    function iterate(object, root = null){
+        for(let key in object){
+            let path = root === null ? key.toString() : `${root.toString()}${separator}${key.toString()}`
+            !isObject(object[key]) ? flatted[path] = object[key] : iterate(object[key],path)
+        }
+    }
+    iterate(object)
+    return flatted
+    }
+    catch(e){console.log(e)}
+}
